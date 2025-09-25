17cb6b55f562be4e03c5119b73c05552e6262a85
Mo Agilah <32136453+MoAgilah@users.noreply.github.com>
2025-09-25 15:41:51 +0100

    Add declaration of GameConstants::Gravity to EngineInterface.

--- Code/Engine/Core/Constants.cpp
+++ Code/Engine/Core/Constants.cpp
@@ -4,6 +4,7 @@ bool  GameConstants::DRender = false;
 bool  GameConstants::GameIsReady = false;
 float GameConstants::FPS = 60.f;
 float GameConstants::ObjectSpeed = 2.f;
+float GameConstants::Gravity = 9.81f;
 Colour GameConstants::WindowColour = Colour(0, 0, 255);
 Vector2f GameConstants::ScreenDim = Vector2f(600.f, 600.f);
 Vector2f GameConstants::Scale = Vector2f(1.f, 1.f);
