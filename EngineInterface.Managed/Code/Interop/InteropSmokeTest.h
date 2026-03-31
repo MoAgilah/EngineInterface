54a1d4e4b8a5b2c83c1cdcfb55935256eb08c795
Mohamed agilah <agilahmohamed@gmail.com>
2026-03-31 20:56:15 +0100

    chore: rename and restructure smoke test as InteropSmokeTest
    - Split into header and implementation files
    - Renamed ManagedSmokeTest to InteropSmokeTest
    - Aligned file naming with class structure

---
+++ EngineInterface.Managed/Code/Interop/InteropSmokeTest.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace EngineInterface
+{
+    namespace Managed
+    {
+        namespace Interop
+        {
+            public ref class InteropSmokeTest
+            {
+            public:
+                static int Add(int a, int b);
+            };
+        }
+    }
+}
