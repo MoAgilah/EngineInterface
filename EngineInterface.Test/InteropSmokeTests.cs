cda750321497b366cd54195451742cb4bed78d59
Mohamed agilah <agilahmohamed@gmail.com>
2026-03-31 20:59:45 +0100

    chore: align interop smoke test namespaces and naming
    - Updated managed wrapper namespace to EngineInterface.Managed.Interop
    - Renamed smoke test class to InteropSmokeTests
    - Moved test namespace to EngineInterface.Tests.Interop

---
+++ EngineInterface.Test/InteropSmokeTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using EngineInterface.Managed.Interop;
+
+namespace EngineInterface.Tests.Interop
+{
+    public class InteropSmokeTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var result = InteropSmokeTest.Add(2, 3);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
