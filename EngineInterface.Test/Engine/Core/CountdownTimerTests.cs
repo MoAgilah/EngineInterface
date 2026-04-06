8a9db79e3bee721b21fc407c6eeee06b8ac850e6
Mohamed agilah <agilahmohamed@gmail.com>
2026-04-06 20:49:29 +0100

    feat(countdownTimer): Add constructor tests for CountdownTimer (valid and edge cases)

---
+++ EngineInterface.Test/Engine/Core/CountdownTimerTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+using EngineInterface.Managed.Engine.Core;
+using Xunit;
+
+namespace EngineInterface.Tests.Engine.Core
+{
+    public class CountdownTimerTests
+    {
+        [Fact]
+        public void Constructor_ShouldSetMaxTimeBeforeCountDown()
+        {
+            var timer = new CountdownTimerWrapper(5.0f);
+
+            var maxTime = timer.GetMaxTime();
+
+            Assert.Equal(5.0f, maxTime);
+        }
+
+        [Fact]
+        public void Constructor_WhenMaxTimeIsZero_TimerStartsAtZero()
+        {
+            var timer = new CountdownTimerWrapper(0.0f);
+
+            var maxTime = timer.GetMaxTime();
+
+            Assert.Equal(0.0f, maxTime);
+        }
+
+        [Fact]
+        public void Constructor_WhenMaxTimeIsZero_CheckEndReturnsTrue()
+        {
+            var timer = new CountdownTimerWrapper(0.0f);
+
+            var res = timer.CheckEnd();
+
+            Assert.True(res);
+        }
+
+        [Fact]
+        public void Constructor_WhenMaxTimeIsNegative_ClampsMaxTimeToZero()
+        {
+            var timer = new CountdownTimerWrapper(-1.0f);
+
+            var maxTime = timer.GetMaxTime();
+
+            Assert.Equal(0.0f, maxTime);
+        }
+
+        [Fact]
+        public void Constructor_WhenMaxTimeIsNegative_StartsAtZero()
+        {
+            var timer = new CountdownTimerWrapper(-1.0f);
+
+            var currTime = timer.GetCurrTime();
+
+            Assert.Equal(0.0f, currTime);
+        }
+    }
+};
