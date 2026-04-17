e54599842402d38d110b04cbf3606e99596f7c57
Mohamed agilah <agilahmohamed@gmail.com>
2026-04-17 13:44:50 +0100

    refactor(engine/ui): replace EnsureValid macros with CheckNotNull

--- Code/Engine/UI/MenuNavigation.cpp
+++ Code/Engine/UI/MenuNavigation.cpp
@@ -1,7 +1,7 @@
 #include "MenuNavigation.h"
 
 #include "../Core/GameManager.h"
-#include "../../Utilities/Utils.h"
+#include "../../Utilities/Guards.h"
 
 MenuNav::MenuNav(KeyCode ascKey, KeyCode desKey)
 	: m_ascKey(ascKey), m_desKey(desKey)
@@ -10,14 +10,16 @@ MenuNav::MenuNav(KeyCode ascKey, KeyCode desKey)
 
 void MenuNav::HandleNavigation()
 {
-	DECL_GET_OR_RETURN(gameMgr, GameManager::Get());
-	DECL_GET_OR_RETURN(inputMgr, gameMgr->GetInputManager())
+	auto* gameMgr = GameManager::Get();
+	if (!CheckNotNull(gameMgr, "Invalid Pointer 'gameMgr' from GameManager::Get()"))
+		return;
 
-	if (inputMgr)
-	{
-		HandleDirection(inputMgr->GetKeyState((int)m_ascKey), m_canAsc, -1);
-		HandleDirection(inputMgr->GetKeyState((int)m_desKey), m_canDes, 1);
-	}
+	auto* inputMgr = gameMgr->GetInputManager();
+	if (!CheckNotNull(inputMgr, "Invalid Pointer 'inputMgr' from gameMgr->GetInputManager()"))
+		return;
+
+	HandleDirection(inputMgr->GetKeyState((int)m_ascKey), m_canAsc, -1);
+	HandleDirection(inputMgr->GetKeyState((int)m_desKey), m_canDes, 1);
 }
 
 void MenuNav::ChangeNavKeys(KeyCode ascKey, KeyCode desKey)
