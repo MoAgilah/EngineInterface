5d4aff3a96586d1227d801b461ded09da81a5102
Mohamed agilah <agilahmohamed@gmail.com>
2026-04-25 12:23:33 +0100

    refactor: Add guarded current movement state updates

--- Code/Utilities/MovementController.h
+++ Code/Utilities/MovementController.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <string>
 #include <map>
 
 struct ToIntFn
@@ -21,27 +20,26 @@ public:
 	void AddAcceleration(int ID, float value);
 	void AddDeceleration(int ID, float value);
 
-	float GetVelocity(int ID);
+	float GetVelocity(int ID) const;
 	void SetCurrentVelLimit(int ID);
-	float GetCurrentVelLimit() { return m_currVelLimit; }
+	float GetCurrentVelLimit() const { return m_currVelLimit; }
 
-	float GetAcceleration(int ID);
+	float GetAcceleration(int ID) const;
 	void SetCurrentAccel(int ID);
-	float GetCurrentAccel() { return m_currAccel; }
+	float GetCurrentAccel() const { return m_currAccel; }
 
-	float GetDeceleration(int ID);
+	float GetDeceleration(int ID) const;
 	void SetCurrentDecel(int ID);
-	float GetCurrentDecel() { return m_currDecel; }
+	float GetCurrentDecel() const { return m_currDecel; }
 
 private:
-
-	float m_currVelLimit = 0;
-	float m_currAccel = 0;
-	float m_currDecel = 0;
+	float m_currVelLimit = 0.0f;
+	float m_currAccel = 0.0f;
+	float m_currDecel = 0.0f;
 
 	std::map<int, float> m_velocities;
 	std::map<int, float> m_accelerations;
-	std::map<int, float> m_decelartions;
+	std::map<int, float> m_decelerations;
 };
 
 class MovementController
@@ -49,19 +47,31 @@ class MovementController
 public:
 	~MovementController() = default;
 
-	void AddMovementXState(int ID, const MovementState& physics);
-	void AddMovementYState(int ID, const MovementState& physics);
+	void AddMovementXState(int ID, const MovementState& state);
+	void AddMovementYState(int ID, const MovementState& state);
 
 	void ChangeMovementXState(int ID);
 	void ChangeMovementYState(int ID);
 
-	MovementState& GetCurrentXState() { return m_currentXState; }
-	MovementState& GetCurrentYState() { return m_currentYState; }
+	const MovementState& GetCurrentXState() const { return m_currentXState; }
+	const MovementState& GetCurrentYState() const { return m_currentYState; }
+
+	void SetCurrentVelLimitForXState(int ID);
+	void SetCurrentVelLimitForYState(int ID);
+
+	void SetCurrentAccelForXState(int ID);
+	void SetCurrentAccelForYState(int ID);
+
+	void SetCurrentDecelForXState(int ID);
+	void SetCurrentDecelForYState(int ID);
 
 private:
+	bool m_hasCurrentXState = false;
+	bool m_hasCurrentYState = false;
 
 	MovementState m_currentXState;
 	MovementState m_currentYState;
+
 	std::map<int, MovementState> m_movementXState;
 	std::map<int, MovementState> m_movementYState;
 };
