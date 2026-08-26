4030243695c115fc2cf8e7c6cbf891109f5bf520
Mohamed agilah <agilahmohamed@gmail.com>
2026-08-26 07:38:53 +0100

    refactor: decouple engine interface from backend types
    - Remove ActiveTypesFwd.cpp from EngineInterface
    - Keep active resource factory declarations backend independent
    - Move active resource factory implementations out of EngineInterface
    - Provide null resource factories through EngineInterface.TestSupport
    - Update ITile to include its required interface dependencies directly
    - Remove reliance on active backend types from interface code

--- Code/Utilities/ActiveTypesFwd.h
+++ Code/Utilities/ActiveTypesFwd.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Defines.h"
 #include "../Engine/Interface/Resources/IFont.h"
 #include "../Engine/Interface/Resources/IMusic.h"
 #include "../Engine/Interface/Resources/IShader.h"
@@ -8,41 +7,8 @@
 #include "../Engine/Interface/Resources/ITexture.h"
 #include <memory>
 
-#if USE_SFML
-
-// Forward-declare the backend wrappers (no heavy includes here)
-class SFFont;
-class SFMusic;
-class SFShader;
-class SFSound;
-class SFTexture;
-
-// Active backend type aliases
-using ActiveFont = SFFont;
-using ActiveMusic = SFMusic;
-using ActiveShader = SFShader;
-using ActiveSound = SFSound;
-using ActiveTexture = SFTexture;
-
-// Factory function declarations (defined in backend)
-std::unique_ptr<IFont>    MakeActiveFont();
-std::unique_ptr<IMusic>   MakeActiveMusic();
-std::unique_ptr<IShader>  MakeActiveShader();
-std::unique_ptr<ISound>   MakeActiveSound();
-std::unique_ptr<ITexture> MakeActiveTexture();
-
-#else
-
-using ActiveFont = IFont;
-using ActiveMusic = IMusic;
-using ActiveShader = IShader;
-using ActiveSound = ISound;
-using ActiveTexture = ITexture;
-
 std::unique_ptr<IFont>    MakeActiveFont();
 std::unique_ptr<IMusic>   MakeActiveMusic();
 std::unique_ptr<ISound>   MakeActiveSound();
 std::unique_ptr<IShader>  MakeActiveShader();
 std::unique_ptr<ITexture> MakeActiveTexture();
-
-#endif
---
+++ EngineInterface.TestSupport/Code/TestHelpers/TestActiveTypes.cpp
@@ -0,0 +1,26 @@
+#include <Utilities/ActiveTypesFwd.h>
+
+std::unique_ptr<IFont> MakeActiveFont()
+{
+    return nullptr;
+}
+
+std::unique_ptr<IMusic> MakeActiveMusic()
+{
+    return nullptr;
+}
+
+std::unique_ptr<ISound> MakeActiveSound()
+{
+    return nullptr;
+}
+
+std::unique_ptr<IShader> MakeActiveShader()
+{
+    return nullptr;
+}
+
+std::unique_ptr<ITexture> MakeActiveTexture()
+{
+    return nullptr;
+}
