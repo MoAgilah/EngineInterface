3632e9d75fd46f7e6fcaa49809e3b050ca71f213
Mohamed agilah <agilahmohamed@gmail.com>
2026-08-29 11:39:32 +0100

    refactor: replace utility macros with guard functions
    - add ThrowIfFalse to Guards
    - remove macro-based validation from Utils
    - simplify error handling with reusable guard functions

--- Code/Utilities/Guards.h
+++ Code/Utilities/Guards.h
@@ -5,7 +5,7 @@
 #include <source_location>
 
 template<typename T>
-bool CheckNotNull(const T* ptr,
+inline bool CheckNotNull(const T* ptr,
     const std::string& msg,
     const std::source_location& loc = std::source_location::current())
 {
@@ -14,4 +14,16 @@ bool CheckNotNull(const T* ptr,
 
     Logger::GetDefaultLogger().Log(LogLevel::Error, msg, loc);
     return false;
+}
+
+inline void ThrowIfFalse(
+    bool expr,
+    const std::string& msg,
+    const std::source_location& loc = std::source_location::current())
+{
+    if (expr)
+        return;
+
+    Logger::GetDefaultLogger().Log(LogLevel::Error, msg, loc);
+    throw std::runtime_error(msg);
 }
